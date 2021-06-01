e6d820d953152aec66f9433335735681d35a8316
Ryan Erricson <ryanerricson@gmail.com>
2021-06-01 16:20:39 -0400

    commit tree e4e0111e741159d5ea3373f35ad5300441fbb097

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,24 @@
 // Code CoordinatesButton Component Here
+import React , {Component} from 'react' 
+
+export default class CoordinatesButton extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            onReceiveCoordinates: this.props.onReceiveCoordinates
+        }
+
+    }
+    handleClick = () => {
+        return [this.props.screenX, this.props.screenY]
+    }
+
+    render(){
+        return (
+            <button
+            onClick = {handleClick}
+            >   
+            </button>
+        )
+    }
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,12 @@
 // Code DelayedButton Component Here
+import React , {Component} from 'react' 
+
+export default class DelayedButton extends Component {
+    constructor(props){
+        super()
+    }
+
+    render(){
+        
+    }
+}
